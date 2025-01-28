37fea1b3d0f5c43e5230001eb37796e15fd9dee9
Shevchik Igor <s.hevchik.mail@gmail.com>
2025-01-28 07:17:33 +0300

    chore: init

--- src/App.vue
+++ src/App.vue
@@ -4,23 +4,24 @@ import PlayCircleIcon from '@bitrix24/b24icons-vue/main/PlayCircleIcon'
 
 <template>
   <B24App>
-    <div class="flex flex-col items-center justify-center gap-4 h-screen">
-      <h1 class="font-bold text-2xl text-blue-500">
+    <div class="flex flex-col items-center justify-center gap-16 h-screen">
+      <h1 class="font-b24-secondary text-8xl font-light">
         Bitrix24 UI - Vue Playground
       </h1>
 
       <div class="flex items-center gap-2">
         <B24Button
+          rounded
           label="Documentation"
           color="ai"
           :icon="PlayCircleIcon"
-          to="https://bitrix24.github.io/b24ui/getting-started/installation/vue"
+          to="https://bitrix24.github.io/b24ui/guide/installation-vue.html"
           target="_blank"
         />
 
         <B24Button
+          rounded
           label="GitHub"
-          icon="i-simple-icons:github"
           to="https://github.com/bitrix24/b24ui"
           target="_blank"
         />
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-
 import bitrix24UIPluginVite  from "@bitrix24/b24ui-nuxt/vite";
 
 // https://vitejs.dev/config/
@@ -8,7 +7,7 @@ export default defineConfig({
   plugins: [
     vue(),
     bitrix24UIPluginVite ({
-      b24ui: {}
+      colorMode: false
     })
   ]
 });
