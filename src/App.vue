a6eeab7dd32fe52c78b8368642a6647f7570ee91
Shevchik Igor <s.hevchik.mail@gmail.com>
2025-01-31 16:49:40 +0300

    chore(template): improve

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,35 @@
 <script setup lang="ts">
-import PlayCircleIcon from '@bitrix24/b24icons-vue/main/PlayCircleIcon'
+import { computed } from 'vue'
+import { useRoute } from 'vue-router'
+
+import LayoutMain from './layout/main.vue'
+import LayoutPage from './layout/page.vue'
+import LayoutSlider from './layout/slider.vue'
+
+const route = useRoute()
+
+const layout = computed(() =>
+{
+  if (route.path === '/') {
+    return LayoutMain
+  }
+  
+  if (route.path.includes('slider')) {
+    return LayoutSlider
+  }
+  
+  if (route.path.includes('slider')) {
+    return LayoutSlider
+  }
+  
+  return LayoutPage
+})
 </script>
 
 <template>
   <B24App>
-    <div class="flex flex-col items-center justify-center gap-16 h-screen">
-      <h1 class="font-b24-secondary text-h1 sm:text-8xl font-light">
-        Bitrix24 UI - Vue Playground
-      </h1>
-
-      <div class="flex flex-wrap items-center gap-2">
-        <B24Button
-          rounded
-          label="Documentation"
-          color="ai"
-          :icon="PlayCircleIcon"
-          to="https://bitrix24.github.io/b24ui/guide/installation-vue.html"
-          target="_blank"
-        />
-
-        <B24Button
-          rounded
-          label="GitHub"
-          color="link"
-          depth="dark"
-          to="https://github.com/bitrix24/b24ui"
-          target="_blank"
-        />
-      </div>
-    </div>
+    <Component :is="layout">
+      <RouterView/>
+    </Component>
   </B24App>
 </template>
---
+++ src/layout/page.vue
@@ -0,0 +1,10 @@
+<template>
+  <header class="relative z-10  border-b border-gray-200">
+    <B24Link to="/">
+      <span class="font-b24-secondary text-h3 font-light">Vue Playground</span>
+    </B24Link>
+  </header>
+  <main>
+    <slot />
+  </main>
+</template>
