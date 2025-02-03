d3c57030351f52b57ad1e9ba73f6f1d548d6778c
Shevchik Igor <s.hevchik.mail@gmail.com>
2025-02-03 07:08:28 +0300

    chore(template): improve

--- src/layout/slider.vue
+++
@@ -1,8 +0,0 @@
-<template>
-	<header class="relative z-10  border-b border-gray-200">
-		Slider
-	</header>
-	<main>
-		<slot />
-	</main>
-</template>
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
 import './assets/main.css'
 import { createApp } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router'
 import b24UiPlugin from '@bitrix24/b24ui-nuxt/vue-plugin'
 import App from './App.vue'
-import router from './router'
 
 const app = createApp(App)
+
+const router = createRouter({
+  routes: [],
+  history: createWebHistory()
+})
 
 app.use(b24UiPlugin)
 app.use(router)
