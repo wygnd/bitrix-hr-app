eaab81b0b8e0bba4fe8d901de48fb90386c3df78
Shevchik Igor <s.hevchik.mail@gmail.com>
2025-08-22 17:06:37 +0300

    feat(AirWeb) improve

--- src/App.vue
+++ src/App.vue
@@ -4,30 +4,32 @@ import PlayCircleIcon from '@bitrix24/b24icons-vue/main/PlayCircleIcon'
 
 <template>
   <B24App>
-    <div class="flex flex-col items-center justify-center gap-16 h-screen">
-      <h1 class="font-b24-secondary text-h1 sm:text-8xl font-light">
-        Bitrix24 UI - Vue Starter
-      </h1>
+    <B24SidebarLayout
+      :use-light-content="false"
+      :b24ui="{ container: 'mt-0' }"
+    >
+      <div class="flex flex-col items-start sm:items-center justify-center gap-[10px] h-[calc(100vh-10px)]">
+        <ProseH1 class="sm:text-8xl font-light">
+          Bitrix24 UI - Vue Starter
+        </ProseH1>
 
-      <div class="flex flex-wrap items-center gap-2">
-        <B24Button
-          rounded
-          label="Documentation"
-          color="ai"
-          :icon="PlayCircleIcon"
-          to="https://bitrix24.github.io/b24ui/guide/installation-vue.html"
-          target="_blank"
-        />
+        <div class="flex flex-wrap items-start sm:items-center gap-2">
+          <B24Button
+            label="Documentation"
+            color="air-primary-copilot"
+            :icon="PlayCircleIcon"
+            to="https://bitrix24.github.io/b24ui/guide/installation-vue.html"
+            target="_blank"
+          />
 
-        <B24Button
-          rounded
-          label="GitHub"
-          color="link"
-          depth="dark"
-          to="https://github.com/bitrix24/b24ui"
-          target="_blank"
-        />
+          <B24Button
+            label="GitHub"
+            color="air-secondary-accent"
+            to="https://github.com/bitrix24/b24ui"
+            target="_blank"
+          />
+        </div>
       </div>
-    </div>
+    </B24SidebarLayout>
   </B24App>
 </template>
