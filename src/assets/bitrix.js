be88d41f94ad3fa610cb3ceeb5e2b3362d144945
benchik <wygndo@gmail.com>
2025-11-02 20:03:11 +0300

    tets

--- src/assets/bitrix.js
+++ src/assets/bitrix.js
@@ -1,11 +1,12 @@
 document.addEventListener('DOMContentLoaded', async () => {
-    try {
-        let BX24 = await B24Js.initializeB24Frame();
-
-        const placementInfo = BX24.placement.info();
-
-        console.log(placementInfo);
-    } catch (error) {
-        // document.querySelector('#app').remove();
-    }
+	console.log(123);
+    // try {
+    //     let BX24 = await B24Js.initializeB24Frame();
+		//
+    //     const placementInfo = BX24.placement.info();
+		//
+    //     console.log(placementInfo);
+    // } catch (error) {
+    //     // document.querySelector('#app').remove();
+    // }
 });
