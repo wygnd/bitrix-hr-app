812d6abe91120c61cf92522cd182b17c50ee0cb0
benchik <wygndo@gmail.com>
2025-11-02 20:16:04 +0300

    add bx sdk

--- src/assets/bitrix.js
+++ src/assets/bitrix.js
@@ -1,12 +1,11 @@
 document.addEventListener('DOMContentLoaded', async () => {
-	console.log(123);
-    // try {
-    //     let BX24 = await B24Js.initializeB24Frame();
-		//
-    //     const placementInfo = BX24.placement.info();
-		//
-    //     console.log(placementInfo);
-    // } catch (error) {
-    //     // document.querySelector('#app').remove();
-    // }
+    try {
+        let BX24 = await B24Js.initializeB24Frame();
+
+        const placementInfo = BX24.placement.info();
+
+        console.log(placementInfo);
+    } catch (error) {
+        document.querySelector('#app').remove();
+    }
 });
