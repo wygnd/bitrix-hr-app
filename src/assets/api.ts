92ccb1d3fca591fa21dc75a52040b27d4c164546
benchik <wygndo@gmail.com>
2025-11-03 12:19:05 +0300

    add bxsdk

--- src/assets/api.ts
+++ src/assets/api.ts
@@ -2,11 +2,11 @@ import axios from 'axios';
 import {BACKEND_API_TOKEN, BACKEND_API_URL} from "../common/config.ts";
 
 const API = axios.create({
-    baseURL: BACKEND_API_URL,
-    headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `bga ${BACKEND_API_TOKEN}`
-    }
+	baseURL: BACKEND_API_URL,
+	headers: {
+		'Content-Type': 'application/json',
+		'Authorization': `bga ${BACKEND_API_TOKEN}`
+	}
 })
 
 export {API};
--- src/assets/bitrix.js
+++ src/assets/bitrix.js
@@ -1,11 +1,11 @@
 document.addEventListener('DOMContentLoaded', async () => {
-    try {
-        let BX24 = await B24Js.initializeB24Frame();
-
-        const placementInfo = BX24.placement.info();
-
-        console.log(placementInfo);
-    } catch (error) {
-        document.querySelector('#app').remove();
-    }
+	try {
+		let BX24 = await B24Js.initializeB24Frame();
+		
+		const placementInfo = BX24.placement.info();
+		
+		console.log(placementInfo);
+	} catch(error) {
+		document.querySelector('#app').remove();
+	}
 });
