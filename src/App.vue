b06d5a020cdd3fb67739468f4f438be1e4ef55a7
wygnd <wygndo@gmail.com>
2025-11-05 16:37:35 +0300

    add bx24 in component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,27 @@
 <script setup lang="ts">
 import Vacancies from './components/vacancies.vue';
+import {B24Frame, initializeB24Frame} from '@bitrix24/b24jssdk'
+import {computed, onMounted, onUnmounted} from "vue";
+
+let $b24: B24Frame
+const isInit = computed(() => $b24?.isInit);
+
+onMounted(async () => {
+  try {
+    $b24 = await initializeB24Frame()
+  } catch (error) {
+    console.error(error)
+  }
+})
+
+onUnmounted(() => {
+  $b24?.destroy()
+})
+
 </script>
 
 <template>
-  <B24App>
+  <B24App v-if="isInit">
     <B24Container>
       <Vacancies/>
     </B24Container>
--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,10 @@ import './assets/main.css'
 import {createApp} from 'vue'
 import b24UiPlugin from '@bitrix24/b24ui-nuxt/vue-plugin'
 import App from './App.vue'
-import {initializeB24Frame} from '@bitrix24/b24jssdk'
 
-initializeB24Frame().then(res => {
-    const $b24 = res;
 
-    const app = createApp(App)
+const app = createApp(App)
 
-    app.use(b24UiPlugin)
+app.use(b24UiPlugin)
 
-    app.mount('#app')
-})
+app.mount('#app')
