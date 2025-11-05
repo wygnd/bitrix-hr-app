dbb74f20a061dddb18513b838b1afbccadbedc5e
wygnd <wygndo@gmail.com>
2025-11-05 16:41:19 +0300

    add bx24 in component

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,18 @@
 <script setup lang="ts">
 import Vacancies from './components/vacancies.vue';
 import {B24Frame, initializeB24Frame} from '@bitrix24/b24jssdk'
-import {computed, onMounted, onUnmounted} from "vue";
+import {onMounted, onUnmounted, ref} from "vue";
 
 let $b24: B24Frame
-const isInit = computed(() => $b24?.isInit);
+const isInit = ref<boolean>(false);
 
 onMounted(async () => {
   try {
     $b24 = await initializeB24Frame()
+    isInit.value = true
   } catch (error) {
     console.error(error)
+    isInit.value = false
   }
 })
 
