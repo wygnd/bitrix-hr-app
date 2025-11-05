4828eaab08569f2ee66cb0e4206f72cb209d2921
wygnd <wygndo@gmail.com>
2025-11-05 17:06:04 +0300

    remove bx sdk init

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,23 @@
 <script setup lang="ts">
 import Vacancies from './components/vacancies.vue';
-import {B24Frame, initializeB24Frame} from '@bitrix24/b24jssdk'
-import {onMounted, onUnmounted, ref} from "vue";
+import {ref} from "vue";
 
-let $b24: B24Frame
-const isInit = ref<boolean>(false);
+// let $b24: B24Frame
+const isInit = ref<boolean>(true);
 
-onMounted(async () => {
-  try {
-    $b24 = await initializeB24Frame()
-    isInit.value = true
-  } catch (error) {
-    console.error(error)
-    isInit.value = false
-  }
-})
-
-onUnmounted(() => {
-  $b24?.destroy()
-})
+// onMounted(async () => {
+//   try {
+//     $b24 = await initializeB24Frame()
+//     isInit.value = true
+//   } catch (error) {
+//     console.error(error)
+//     isInit.value = false
+//   }
+// })
+//
+// onUnmounted(() => {
+//   $b24?.destroy()
+// })
 
 </script>
 
