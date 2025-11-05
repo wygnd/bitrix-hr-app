11f6ca8301701364143f00a86797cd847aa4b8f5
wygnd <wygndo@gmail.com>
2025-11-05 15:22:17 +0300

    add bxsdk. trying init frame

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,14 @@ import './assets/main.css'
 import {createApp} from 'vue'
 import b24UiPlugin from '@bitrix24/b24ui-nuxt/vue-plugin'
 import App from './App.vue'
+import {initializeB24Frame} from '@bitrix24/b24jssdk'
 
-const app = createApp(App)
+initializeB24Frame().then(res => {
+    const $b24 = res;
 
-app.use(b24UiPlugin)
+    const app = createApp(App)
 
-app.mount('#app')
+    app.use(b24UiPlugin)
+
+    app.mount('#app')
+})
